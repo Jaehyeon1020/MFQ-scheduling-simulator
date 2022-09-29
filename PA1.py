5cef695d0ad55579c89322dad1b6adfe129635b5
Jaehyeon1020 <kjhkk1020@naver.com>
2022-09-29 22:56:50 +0900

    동작 테스트 끝, 출력 형식 만들어야됨

--- PA1.py
+++ PA1.py
@@ -18,12 +18,12 @@ from collections import deque
 
 class Process:
     def __init__(self, pid, arrival_time, init_queue, cycles, sequence):
-        self.__pid = pid
-        self.__arrival_time = arrival_time
-        self.__init_queue = init_queue
-        self.__cycles = cycles
+        self.__pid = int(pid)
+        self.__arrival_time = int(arrival_time)
+        self.__init_queue = int(init_queue)
+        self.__cycles = int(cycles)
         self.sequence = sequence
-        self.current_queue = init_queue
+        self.current_queue = int(init_queue)
         self.turnaround_time = 0  # completion time - arrival time
         self.waiting_time = 0  # queue에 있는 총 시간
         self.starting_time = 0  # cpu 처음 할당 받을 때 시간
@@ -76,25 +76,13 @@ class Process:
         self.__cycles = cycles
 
     def print_process(self):
-        print("Process PID: " + self.__pid)
-        print("Arrival Time: " + self.__arrival_time)
-        print("Init Queue: " + self.__init_queue)
-        print("Current Queue: " + self.current_queue)
-        print("# of Cycles: " + self.__cycles)
+        print("Process PID: %d" % self.__pid)
+        print("Arrival Time: %d" % self.__arrival_time)
+        print("Init Queue: %d" % self.__init_queue)
+        print("Current Queue: %d" % self.current_queue)
+        print("# of Cycles: %d" % self.__cycles)
         print("Sequences: ", end='')
         print(self.sequence)
-        print("")
-"""
-    def preemption(self):
-        self.process_status = 1
-        if self.current_queue < 2:
-            self.current_queue += 1
-
-    def wakeup(self):
-        self.process_status = 1
-        if self.current_queue > 0:
-            self.current_queue -= 1
-"""
 
 
 def split_process_info(process_info_line):
@@ -103,6 +91,8 @@ def split_process_info(process_info_line):
 
 
 def mfq(process_list):
+    print("mfq(): function start")
+    scheduling_result = []  # 스케줄링 순서 저장
     ready_queue_0 = deque([])  # ready queue 0
     ready_queue_1 = deque([])  # ready queue 1
     ready_queue_2 = deque([])  # ready queue 2
@@ -114,6 +104,8 @@ def mfq(process_list):
     def first_insert_rq(process: Process):
         # process의 init_queue값에 맞게 ready queue에 삽입, in-queue status로 전환
         # 처음 들어올때 cycle 1의 cpu burst, IO burst 값 부여 해야함
+        print("called first_insert_rq")  # test
+
         if process.init_queue == 0:
             process.timeslice = 2  # timeslice 부여
             process.process_status = 1  # 프로세스 상태 in-queue로 전환
@@ -136,10 +128,14 @@ def mfq(process_list):
         # process가 원래 있었던 큐로 돌려보내줌, timeslice 새로 세팅, in-queue status로 전환
         # I\O가 끝났다는 것은 1 cycle이 끝났다는 뜻 -> cycle 증가
         # wakeup 할때마다 cycle이 끝나므로 새로운 cycle의 cpu burst, IO burst값 부여해야함
+        print("called wakeup")  # test
+
         if process.current_queue == 0:
             process.timeslice = 2  # timeslice set
             process.process_status = 1  # process status set
             process.current_cycle += 1  # current cycle ++
+            print("wakeup : current cycle ++") # test
+            print("current cycle : %d" % process.current_cycle)
             process.remaining_cpu_burst = int(process.sequence[(process.current_cycle-1)*2])  # new cpu burst time set
             if process.current_cycle != process.cycles:  # 마지막 사이클인경우는 io burst는 설정 안함
                 process.remaining_io_burst = int(process.sequence[(process.current_cycle-1)*2 + 1])  # new io burst time set
@@ -149,6 +145,7 @@ def mfq(process_list):
             process.timeslice = 4
             process.process_status = 1
             process.current_cycle += 1
+            print("wakeup : current cycle ++")  # test
             process.remaining_cpu_burst = int(process.sequence[(process.current_cycle - 1) * 2])  # new cpu burst time set
             if process.current_cycle != process.cycles:  # 마지막 사이클인경우는 io burst는 설정 안함
                 process.remaining_io_burst = int(
@@ -158,6 +155,7 @@ def mfq(process_list):
         else:
             process.process_status = 1
             process.current_cycle += 1
+            print("wakeup : current cycle ++")  # test
             process.remaining_cpu_burst = int(process.sequence[(process.current_cycle - 1) * 2])  # new cpu burst time set
             if process.current_cycle != process.cycles:  # 마지막 사이클인경우는 io burst는 설정 안함
                 process.remaining_io_burst = int(
@@ -167,54 +165,80 @@ def mfq(process_list):
     def shortest_remaining():
         # Q2의 SRNT 스케줄링을 위한 함수
         # Q2에서 가장 남은 cpu burst time이 짧은 프로세스를 반환
+        print("called shortest_remaining")  # test
+
         shortest = ready_queue_2[0]
         for i in range(1, len(ready_queue_2)):
-            if ready_queue_2[i].remaining_cpu_burst < shortest.remainig_cpu_burst:
+            if ready_queue_2[i].remaining_cpu_burst < shortest.remaining_cpu_burst:
                 shortest = ready_queue_2[i]
         return shortest
 
     def fetch():
         # process fetch, in-cpu status로 전환
+        print("called fetch")  # test
+
         nonlocal current_queue
         nonlocal current_process
 
         if len(ready_queue_0) > 0:
-            if current_queue == 2: # q2 process가 돌고있는 경우
+            print("fetch() : if len(ready_queue_0) > 0")  # test
+            if current_queue == 2 and current_process is not None:  # q2 process가 돌고있는 경우
                 preemption(current_process)  # q2 프로세스 내보냄
 
             current_queue = 0  # 현재 큐를 q0로
             current_process = ready_queue_0.pop()  # q0의 프로세스를 cpu에 올림
             current_process.process_status = 2  # 올려진 프로세스의 status : in-cpu
+            scheduling_result.append(current_process)
 
             if current_process.is_been_cpu is False:  # cpu에 한번도 올라간 적이 없으면
                 current_process.starting_time = time  # starting time 기록
                 current_process.is_been_cpu = True
 
         elif len(ready_queue_1) > 0:
-            if current_queue == 2: # q2 process가 돌고있는 경우
+            print("fetch() : elif len(ready_queue_1) > 0")  # test
+            if current_queue == 2 and current_process is not None: # q2 process가 돌고있는 경우
                 preemption(current_process)  # q2 프로세스 내보냄
 
             current_queue = 1
             current_process = ready_queue_1.pop()
             current_process.process_status = 2
+            scheduling_result.append(current_process)
+
+            if current_process.is_been_cpu is False:  # cpu에 한번도 올라간 적이 없으면
+                current_process.starting_time = time  # starting time 기록
+                current_process.is_been_cpu = True
 
         elif len(ready_queue_2) > 0:  # SRTN 스케줄링 처리와 이미 current process = None이 된 경우 구분해서 구현
+            print("fetch() : elif len(ready_queue_2) > 0")  # test
             current_queue = 2
             shortest = shortest_remaining()  # 남은 cpu burst time 가장 적은 process 계산
 
             if current_process is None:  # 이미 current process = None이 된 경우(이미 preemption or sleep된 경우)
                 current_process = shortest
                 current_process.process_status = 2
+                scheduling_result.append(current_process)
                 ready_queue_2.remove(shortest)
-            elif current_process.remaining_cpu_burst > shortest.remainig_cpu_burst:
+
+                if current_process.is_been_cpu is False:  # cpu에 한번도 올라간 적이 없으면
+                    current_process.starting_time = time  # starting time 기록
+                    current_process.is_been_cpu = True
+
+            elif current_process.remaining_cpu_burst > shortest.remaining_cpu_burst:
                 # 현재 cpu에 올라가있는 프로세스가 더 cpu 시간 많이남았으면 교체(SRTN)
                 preemption(current_process)
                 current_process = shortest
                 current_process.process_status = 2
+                scheduling_result.append(current_process)
                 ready_queue_2.remove(shortest)
+
+                if current_process.is_been_cpu is False:  # cpu에 한번도 올라간 적이 없으면
+                    current_process.starting_time = time  # starting time 기록
+                    current_process.is_been_cpu = True
 
     def insert_queue(pc: Process):
         # 인자로 받은 pc를 큐에 저장, in-queue status로 전환, timeslice 새로 부여
+        print("called insert_queue")  # test
+
         if pc.current_queue == 0:
             pc.process_status = 1
             pc.timeslice = 2
@@ -229,12 +253,17 @@ def mfq(process_list):
 
     def preemption(pc: Process):
         # process 상태를 in-queue로 바꾸고 한단계 낮은 큐로 진입시킴
+        print("called preemption")  # test
+        print("preempted :", end='')  # test
+        print(pc)
+
         pc.process_status = 1
         if pc.current_queue != 2:
             pc.current_queue += 1
         insert_queue(pc)
 
     def check_all_over():
+        print("called check_all_over")
         all_over = True
 
         for pc in process_list:
@@ -242,6 +271,7 @@ def mfq(process_list):
                 all_over = False
 
         if all_over is True:
+            print("check_all_over(): all processes over")
             return True
         else:
             return False
@@ -252,7 +282,17 @@ def mfq(process_list):
                 current_process = pc  # cpu에 바로 올림
                 current_queue = current_process.init_queue  # 현재 실행중 큐를 이 프로세스의 시작 큐로
                 current_process.starting_time = 0  # 최초로 cpu에 올라간 시간 time=0
+                current_process.process_status = 2
+                current_process.remaining_cpu_burst = int(current_process.sequence[0])  # first cpu burst set
+                current_process.remaining_io_burst = int(current_process.sequence[1])  # first io burst set
+                current_process.is_been_cpu = True
+                scheduling_result.append(current_process)  # 스케줄링 결과에 기록
 
+                if current_queue == 0:
+                    current_process.timeslice = 2
+                elif current_queue == 1:
+                    current_process.timeslice = 4
+                break
 
     while True:
         """
@@ -275,46 +315,70 @@ def mfq(process_list):
             - 위의 결과로 cpu가 비었다면 새로 fetch
             - time ++
         """
-        if check_all_over():  # 모든 프로세스가 종료되었다면 mfq 함수 종료(return 1)
-            return 1
+        print("----------enter while------------")  # test
+        print("time : %d" % time)  # test
+        print(current_process)  # test
 
-        if current_process.current_queue == 0 or current_process.current_queue == 1:  # 현재 cpu 점유 프로세스가 q0/q1에서 왔다면
-            if current_process.timeslice > 0:  # timeslice 1 이상 남았는지 확인해서
-                current_process.timeslice -= 1  # 남았으면 -1
+        if check_all_over():  # 모든 프로세스가 종료되었다면 mfq 함수 종료(return 1)
+            break
+
+        if current_process is not None:  # 현재 프로세스가 있을 때만 실행
+            if current_process.current_queue == 0 or current_process.current_queue == 1:  # 현재 cpu 점유 프로세스가 q0/q1에서 왔다면
+                print("timeslice check")  # test
+                if current_process.timeslice > 0:  # timeslice 1 이상 남았는지 확인해서
+                    print("timeslice --")  # test
+                    current_process.timeslice -= 1  # 남았으면 -1
 
         # 모든 프로세스에 대해 각 time마다 확인
         for process in process_list:
+            print("for process in process_list 진입")  # test
             # 현재 IO burst 중인 프로세스의 IO burst time --(0보다 클 때)
             if process.process_status == 3 and process.remaining_io_burst > 0:
+                print("io burst --")  # test
                 process.remaining_io_burst -= 1
             # 현재 cpu burst 중인 프로세스의 cpu burst time --(0보다 클 때)
             if process.process_status == 2 and process.remaining_cpu_burst > 0:
+                print("cpu burst --")  # test
                 process.remaining_cpu_burst -= 1
 
             # arrival time 된 프로세스 rq 진입
-            if process.arrival_time == time:
+            if process.arrival_time == time and time > 0:
                 first_insert_rq(process)
             # IO burst 끝난 프로세스 rq 진입
             if process.process_status == 3 and process.remaining_io_burst == 0:
                 wakeup(process)
             # 이 프로세스가 현재 cpu 점유중이라면
             if process is current_process:
-                if process.remaining_cpu_burst == 0:  # cpu burst time 0이면
+                print("this process is current_process")  # test
+                if process.remaining_cpu_burst == 0:  # cpu burst time = 0이면
+                    print("and remaining cpu burst == 0")  # test
                     if process.current_cycle == process.cycles:  # 현재 마지막 cycle이었다면 종료상태로 전환
+                        print("and at last cycle")  # test
+                        print("Process completed")
                         process.process_status = -1
                         process.completion_time = time  # 종료상태 전환 후 completion time 기록
                         current_process = None  # 현재 프로세스 지우기
+                        print("current process setted None")  # test
                     else:  # 마지막 cycle 아니었다면 IO burst 상태로 전환
+                        print("and not at last cycle")  # test
+                        print("converted to IO burst status")  # test
                         process.process_status = 3
                         current_process = None  # 현재 프로세스 지우기
+                        print("current process setted None")  # test
                 else:  # cpu burst time 0 아니면
-                    if process.current_queue == 0 or process.current_queue == 1:  # 이 cpu가 q0 또는 q1에서 왔는지 확인
+                    print("and remaining cpu burst != 0")  # test
+                    if process.current_queue == 0 or process.current_queue == 1:  # 이 process가 q0 또는 q1에서 왔는지 확인
+                        print("and this process is from q0 or q1")  # test
                         if process.timeslice == 0:  # q0/q1에서 왔으면 timeslice가 0이 됐는지 확인
+                            print("and remaining timeslice 0")  # test
                             preemption(process)  # timeslice 0이라면 preemption
                             current_process = None  # 현재 프로세스 지우기
+                            print("current process setted None")  # test
+            print("for process in process_list 끝")
 
         if (current_process is None) or (current_process.current_queue == 2):
             # 위 for 문의 결과로 cpu에서 preemption 또는 sleep으로 전환되어 cpu가 비었을 때 또는 q2에서 온 프로세스가 돌고있을 때
+            print("if current process is None or current process.current queue == 2")  # test
             fetch()  # 새로운 프로세스 cpu에 올림(q2의 경우 상위 큐에 프로세스가 들어왔거나 같은 큐에 더 시간 짧은 프로세스가 있을 때 바뀜)
 
         # 현재 ready queue 내에 존재하는 프로세스의 wating time ++
@@ -326,7 +390,19 @@ def mfq(process_list):
             in_rq_process.waiting_time += 1
 
         time += 1  # time 1 증가, while loop 끝
-    #### 함수 끝날 때 프로세스마다 갖고있는 시간 전부 계산해야됨
+        print("----------while END------------")  # test
+
+    for pc in process_list:
+        pc.turnaround_time = pc.completion_time - pc.arrival_time
+        print("turnaround time: %d" % pc.turnaround_time)  # test
+
+    for i in range(0,len(scheduling_result)):
+        scheduling_result[i] = scheduling_result[i].pid
+
+    print("scheduling result: ", end='')
+    print(scheduling_result)
+    print("mfq(): function over")  # test
+    return 1
 
 
 try:
@@ -355,4 +431,13 @@ for i in range(0, number_of_processes):
 for i in range(0, len(process_list)):
     process_list[i].sequence = process_list[i].sequence.split()
 
+for pc in process_list:
+    for i in range(0, len(pc.sequence)):
+        pc.sequence[i] = int(pc.sequence[i])
+
+for pc in process_list:
+    pc.print_process()  # test
+    print("")
+
+print("----------------main-----------------\n")  # test
 mfq(process_list)
